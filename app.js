4b68de2fd617a0efc943d430380a1ea34463564f
samslezek <sdotslezek@gmail.com>
2019-11-26 21:45:48 -0800

    added my ratchet piano

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	indexRoutes = require("./routes/index"),
 	Blogpost  = require("./models/blogpost"),
-	seedDB = require('./seeds');
+	seedDB = require('./seeds'),
+	tone = require('tone');
 
 mongoose.connect('mongodb://testuser:testpassword1@ds341825.mlab.com:41825/samsblog', { useNewUrlParser: true });
 app.set("view engine", "ejs");
-seedDB(); 
+app.use(express.static(__dirname + "/public"));
+// seedDB(); 
 
 var port = process.env.PORT || 3000;
 
--- routes/index.js
+++ routes/index.js
@@ -15,41 +15,13 @@ router.get("/", function(req, res){
     });
 });
 
-router.get("/posts/:id", function(req, res){
-    Blogpost.findById(req.params.id, function(err, foundBlogpost){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("I found a blog post.")
-            res.render('show', {blogpost: foundBlogpost});
-        }
-    });
-});
-
-router.get("/posts", function(req, res){
-    Blogpost.find({}, function(err, allBlogposts){
-       if(err){
-           console.log(err);
-       } else {
-          allBlogposts.sort(function(a,b){
-            if (err){
-              console.log(err);
-            } else {
-              console.log('sorting')
-              return new Date(a.date).getTime() - new Date(b.date).getTime();
-            }
-          });
-          res.render('posts',{blogposts:allBlogposts});
-          allBlogposts.forEach(function(blogpost){
-            console.log(blogpost.title)
-          })
-       }
-    });
-});
-
 router.get("/about", function(req, res){
-    //find the campground with provided ID
     res.render('about')
 });
+
+router.get("/music", function(req, res){
+    res.render('music')
+});
+
 
 module.exports = router;
