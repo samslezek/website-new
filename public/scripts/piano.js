b30dbe84fcbaf560c1bb992b9b53f7dcfeae0dc7
samslezek <sdotslezek@gmail.com>
2019-11-29 19:13:45 -0800

    removing potentially redundant code

--- public/scripts/piano.js
+++ public/scripts/piano.js
@@ -17,10 +17,10 @@ piano.addEventListener("mousedown", e => {
   synth.triggerAttack(e.target.dataset.note);
 });
 
-piano.addEventListener("touchstart", e => {
-  // try to get this to work on mobile
-  synth.triggerAttack(e.target.dataset.note);
-});
+// piano.addEventListener("touchstart", e => {
+//   // try to get this to work on mobile
+//   synth.triggerAttack(e.target.dataset.note);
+// });
 
 piano.addEventListener("mouseup", e => {
   // stops the trigger
@@ -80,20 +80,20 @@ document.addEventListener("keyup", e => {
 });
 
   // try to get this to work on mobile
-document.addEventListener("touchend", e => {
-  switch (e.key) {
-    case "d":
-    case "r":
-    case "f":
-    case "t":
-    case "g":
-    case "h":
-    case "u":
-    case "j":
-    case "i":
-    case "k":
-    case "o":
-    case "l":
-       synth.triggerRelease(); 
-  }
-});
+// document.addEventListener("touchend", e => {
+//   switch (e.key) {
+//     case "d":
+//     case "r":
+//     case "f":
+//     case "t":
+//     case "g":
+//     case "h":
+//     case "u":
+//     case "j":
+//     case "i":
+//     case "k":
+//     case "o":
+//     case "l":
+//        synth.triggerRelease(); 
+//   }
+// });
